d485d72adebf244cb528921fead719f825e37303
samiraliyev <samir.aliyev@code.edu.az>
2023-03-13 23:02:05 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,115 +1,55 @@
 
-
 // for keep the DOM elements
-let price1 = document.querySelector(".price1");
-let count1 = document.querySelector(".count1");
-let total1 = document.querySelector(".total1");
-let increment1 = document.querySelector(".increment1");
-let decrement1 = document.querySelector(".decrement1");
+let price = document.querySelectorAll(".price");
+let count = document.querySelectorAll(".count");
+let total = document.querySelectorAll(".total");
+let increment = document.querySelectorAll(".increment");
+let decrement = document.querySelectorAll(".decrement");
 let totalSum = document.querySelector(".totalSum");
 
-let price2 = document.querySelector(".price2");
-let count2 = document.querySelector(".count2");
-let total2 = document.querySelector(".total2");
-let increment2 = document.querySelector(".increment2");
-let decrement2 = document.querySelector(".decrement2");
 
+let newCount = 1;
 
-
-// let price3 = document.getElementsByTagName("input")[0].value;
-// console.log(price3);
-
-
-// console.log(price);
-// console.log(count);
-// console.log(total);
-// console.log(increment);
-// console.log(decrement);
-// console.log(totalSum);
-
-
-// for assign values to price, count and total.
-let newPrice1 = 100;
-price1.innerHTML = newPrice1;
-let newCount1 = 1;
-count1.innerHTML = newCount1;
-total1.innerHTML = newPrice1;
-
-let newPrice2 = 200;
-price2.innerHTML = newPrice2;
-let newCount2 = 1;
-count2.innerHTML = newCount2;
-total2.innerHTML = newPrice2;
-
-let newTotalSum = 0;
-totalSum.innerHTML = newTotalSum;
-
-
-let sums = [];
-
-let newSums = sums.push(total1, total2);
-
-     newSums = sums.reduce((acc,item)=>acc + sums);
-    console.log(newSums);
-
-increment1.addEventListener("click", ()=> {
-    newCount1 ++;
-    count1.innerHTML = newCount1;
-    total1.innerHTML = newCount1 * newPrice1;
-   if(newCount1 >= 1){
-    decrement1.removeAttribute("disabled"); 
-    decrement1.style.backgroundColor = "chartreuse";
-    decrement1.style.pointerEvents = "auto";
-   }
-   
-})
-increment2.addEventListener("click", ()=> {
-
-    newCount2 ++;
-    count2.innerHTML = newCount2;
-    total2.innerHTML = newCount2 * newPrice2;
-
-   if(newCount2 >= 1){
-    decrement2.removeAttribute("disabled");
-    decrement2.style.backgroundColor = "chartreuse";
-    decrement2.style.pointerEvents = "auto";
-
-   }
-
-})
-
-
-
-
-
-decrement1.addEventListener("click", ()=>{
-
+for (let i = 0; i < increment.length; i ++){
+    total[i].innerHTML = price[i].textContent;
     
-    if(newCount1 <= 1){
-       decrement1.setAttribute("disabled","");
-       decrement1.style.backgroundColor = "gray";
-       decrement1.style.pointerEvents = "none";
+    increment[i].addEventListener("click", (event)=>{
+        newCount ++;
+        let eventElement = event.target;
+        let newPrice = eventElement.parentNode.parentNode.children[0].textContent;
+        let newTotal = newPrice * newCount;
+        let defaultTotal = eventElement.parentNode.parentNode.children[2].textContent;
+        defaultTotal.innerHTML = newPrice.toString();
+        total[i].innerHTML = newTotal;
+        count[i].innerHTML = newCount;
+        updateTotal();
+    });
+}
 
-    }else{
-        newCount1 --;
-        count1.innerHTML = newCount1;
-        total1.innerHTML = newCount1 * newPrice1;
+for (let i = 0; i < increment.length; i ++){
+    decrement[i].addEventListener("click", (event)=>{
+        newCount --;
+        let eventElement = event.target;
+        let newPrice = eventElement.parentNode.parentNode.children[0].textContent;
+        let newTotal = newPrice * newCount;
+        let defaultTotal = eventElement.parentNode.parentNode.children[2];
+        defaultTotal.innerHTML = newPrice.toString();
+        total[i].innerHTML = newTotal;
+        count[i].innerHTML = newCount;
+        updateTotal();
+    });
+}
+
+
+
+
+
+function updateTotal() {
+    let totalPrice = 0;
+    for (let i = 0; i < total.length; i++) {
+        let itemTotal = parseFloat(total[i].textContent);
+        totalPrice += itemTotal;
     }
-})
-
-decrement2.addEventListener("click", ()=>{
-
-    
-    if(newCount2 <= 1){
-       decrement2.setAttribute("disabled","");
-       decrement2.style.backgroundColor = "gray";
-       decrement2.style.pointerEvents = "none";
-
-    }else{
-        newCount2 --;
-        count2.innerHTML = newCount2;
-        total2.innerHTML = newCount2 * newPrice2;
-    }
-})
-
+    totalSum.innerHTML = totalPrice;
+}
 
